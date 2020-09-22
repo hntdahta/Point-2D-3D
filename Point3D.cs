f9b95124c9632b99149e627edad4e0f29523ff07
Thanh Dat <datmathoingu@gmail.com>
2020-09-22 12:50:54 +0700

    Initial commit

---
+++ Point3D.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Point_2D_3D
+{
+    class Point3D : Point2D
+    {
+        public float z = 0.0f;
+        public Point3D(float x, float y, float z) : base(x, y)
+        {
+            ;
+            this.z = z;
+        }
+        public Point3D() { }
+        public float GetZ()
+        {
+            return z;
+        }
+
+        public void SetZ(float z)
+        {
+            this.z = z;
+        }
+        public float[] GetXYZ()
+        {
+            return new float[3] { base.GetX(), base.GetY(), z };
+        }
+
+        public void SetXYZ(float z)
+        {
+
+            this.z = z;
+        }
+        public override string ToString()
+        {
+            return "x = " + GetX() + "\ny = " + GetY() + "\nz = " + GetZ() + "\n(x,y,z) = " + string.Join(", ", GetXYZ());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Point_2D_3D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Point 2D");
+            Point2D point2D = new Point2D(5.6f, 12.11f);
+            Console.WriteLine(point2D.ToString());
+            Console.WriteLine();
+            Console.WriteLine("Point 3D");
+            Point3D point3D = new Point3D(6.6f, 1.2f, 1.1f);
+            Console.WriteLine(point3D.ToString());
+        }
+    }
+}
